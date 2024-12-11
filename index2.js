25dddd9949cd6b14bb01e9ac68c59df1b32089a5
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2024-12-11 18:07:53 +0800

    commit

--- index2.js
+++ index2.js
@@ -57,7 +57,7 @@ console.log(totalAmount(100, "good"))
 
 function splitAmount(amount, service, people){
     if (service=="good"){
-        return (amount+amount*(20/100))/people
+        return (amount+amount*0.2)/people
     }else if (service=="fair"){
         return (amount+amount*(15/100))/people
     }else if(service=="poor"){
@@ -126,6 +126,19 @@ function rockPaperScissors(player1, player2){
 // Examples:
 // numberJoinerWhile(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerWhile(12, 14) --> '12_13_14'
+
+function numberJoinerWhile(number1 ,number2){
+    let joiner1=""
+    let i = number1
+    do {
+        joiner1+=i+" ";
+        i++;
+    }
+    while (i<=number2);
+    let joiner = (joiner1.trimEnd()).replaceAll(" ", "_")
+    return joiner
+}
+console.log(numberJoinerWhile(5, 9))
  
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "numberJoinerFor" which does the same thing as "numberJoinerWhile",
@@ -136,9 +149,9 @@ function numberJoinerFor(number1, number2){
     for (let i = number1; i<=number2; i++){
         joiner1 += i+" "
     }
-    const joiner = (joiner1.toString()).split("_")
+    const joiner = ((joiner1.toString()).trimEnd()).replaceAll(" ", "_")
     return joiner
-}console.log(numberJoinerFor(3, 5))
+}console.log(numberJoinerFor(1, 10))
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "numberJoinerFancy" which does the same thing as "numberJoinerWhile",
@@ -149,6 +162,15 @@ function numberJoinerFor(number1, number2){
 // numberJoinerFancy(1, 5, '~') --> 1~2~3~4~5
 // numberJoinerFancy(3, 6, '***BANANAS***') --> 1***BANANAS***2***BANANAS***3
  
+function numberJoinerFancy(number1, number2,separator ){
+    let joiner1 = ""
+    for (let i = number1; i<=number2; i++){
+        joiner1 += i+" "
+    }
+    const joiner = ((joiner1.toString()).trimEnd()).replaceAll(" ", separator)
+    return joiner
+}console.log(numberJoinerFancy(1, 10, "***BANANAS***"))
+
 // Exercise 7
  
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -156,6 +178,15 @@ function numberJoinerFor(number1, number2){
 //
 // Example:
 // reverse("skoob") --> "books"
+
+function reverse(word){
+    let str = ""
+    for (let i = word.length-1; i>=0; i--){
+        str += word[i]
+    }
+    return str
+}
+console.log(reverse("rover"))
  
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "findLongestWord" that takes a string of words and returns
@@ -165,6 +196,18 @@ function numberJoinerFor(number1, number2){
 // Example:
 // findLongestWord('a book full of dogs') --> 'book'
  
+function findLongestWord(string){
+    string = string.match(/[a-zA-Z0-9]+/gi);
+    let longest = "";
+    for (let i = 0; i<string.length;i++){
+        if (string[i].length>longest.length){
+            longest = string[i]
+        }
+    }
+    return longest
+}
+console.log(findLongestWord("a book full dogs"))
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function called "nicer"
 // It should clean up the language in its input sentence.
@@ -174,6 +217,21 @@ function numberJoinerFor(number1, number2){
 // nicer('mom get the heck in here and bring me a darn sandwich.')
 // > 'mom get the in here and bring me a sandwich.'
  
+function nicer(str){
+    forBiddenWords = ["heck", "darn", "crappy"]
+    let str1 = str.split(" ")
+    for ( let i=forBiddenWords.length;i>=0; i--){
+        let str1Len = str1.length
+        for (let j =0; j<str1Len; j++){
+            if (str1[j]==forBiddenWords[i]){
+                str1 = str1.slice(0, j).concat(str1.slice(j+1, str1Len));
+            }
+        }
+    }
+return str1.toString().replaceAll(",", " ")
+}
+console.log(nicer('mom get the heck in here and bring me a darn sandwich.'))
+
 // Exercise 8
  
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -184,7 +242,18 @@ function numberJoinerFor(number1, number2){
 // Examples:
 // capitalizeAll('hello world') --> 'Hello World'
 // capitalizeAll('every day is like sunday') --> 'Every Day Is Like Sunday'
- 
+
+function capitalizeAll(str){
+    let newArray = str.split(" ")
+    let result = ''
+    for (let i =0; i<newArray.length; i++){
+        let newWord = newArray[i][0].toUpperCase() + newArray[i].slice(1)
+        result += newWord+" "
+    }
+    return result
+}
+console.log(capitalizeAll("every day is like sunday"))
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function called "split" that does the same thing as String.split
 // It should take two inputs: (1) a string and (2) a delimiter string
@@ -196,17 +265,45 @@ function numberJoinerFor(number1, number2){
 // split('APPLExxBANANAxxCHERRY', 'xx') --> ['APPLE', 'BANANA', 'CHERRY']
 // split('xyz', 'r') --> ['xyz']
  
+function split(array, delimiter){
+    let str = array.toString().replaceAll(delimiter," ")
+    console.log(str)
+    let arr = ['']
+    let j = 0
+    for (let i = 0; i<str.length; i++){
+        if (str[i]==" "){
+            j++;
+            arr.push('');
+        }else{
+            arr[j] += str[i]
+        }
+    }
+    return arr
+}
+console.log(split("APPLExxBANANAxxCHERRYxxBLUEBERRY", "xx"))
+
 // Exercise 9
  
 // Write a function "max" that takes an array of numbers returns the highest
 // number in the array.
  
+
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "sumNumbers" which is given an array of numbers and returns
 // the sum of the numbers.
 // Example:
 // sumNumbers([1, 4, 8]) --> 13
  
+function sumNumbers(arr){
+    let sum = 0
+    for (let i = 0; i<arr.length; i++){
+        sum +=(arr[i])
+    }
+    return sum
+}
+console.log(sumNumbers([1, 2, 3, 5]))
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "positives" which is given an array of numbers and
 // returns a new array containing only the positive numbers within the given array.
@@ -214,6 +311,12 @@ function numberJoinerFor(number1, number2){
 // positives([1, -3, 5, -3, 0]) --> [1, 5]
 // positives([1, 2, 3]) --> [1, 2, 3]
 // positives([-1, -2, -3]) --> []
+function positives(arr){
+    for (let i = 0; i<arr.length; i++){
+        if ((arr[i]>0))
+    }
+}
+console.log(positives([1, -3, -2, 3, 5]))
  
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "evens" which takes an array of numbers and returns a new
